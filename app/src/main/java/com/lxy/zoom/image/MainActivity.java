750777f3328ead4169ff127146f0210366f03f9a
lxykad <503215875@qq.com>
2017-10-19 23:21:38 +0800

    add scaletype test

--- app/src/main/java/com/lxy/zoom/image/MainActivity.java
+++ app/src/main/java/com/lxy/zoom/image/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +16,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void goToBigImg(View view) {
         startActivity(new Intent(view.getContext(), BigImgActivity.class));
+    }
+    public void goType(View view) {
+        startActivity(new Intent(view.getContext(), ScaleTypeActivity.class));
     }
 }
